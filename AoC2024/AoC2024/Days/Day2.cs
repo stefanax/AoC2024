7a828be1d11692089c8cf27a783eb2e5067437cb
Stefan Axelsson <stefanax@hotmail.com>
2024-12-05 10:12:29 +0100

    Day2. Turns out Remove() and RemoveAt() ain't the same. Who would have thunk it?

---
+++ AoC2024/AoC2024/Days/Day2.cs
@@ -0,0 +1,123 @@
+﻿namespace AoC2024;
+
+public class Day2
+{
+    private readonly InputFiles _inputFiles = new InputFiles();
+
+    public void Step1()
+    {
+        var input = _inputFiles.ReadInputFileForDay(2, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        var reports = new List<List<int>>();
+
+        foreach (var inputItem in inputList)
+        {
+            var inputValues = inputItem.Split(" ").Select(int.Parse).ToList();
+            reports.Add(inputValues);
+        }
+
+        var safeReportsCount = 0;
+
+        foreach (var report in reports)
+        {
+            var directionIncrease = report[0] < report[1];
+
+            var allGood = true;
+
+            for (var i = 0; i < report.Count - 1; i++)
+            {
+                var result = 0;
+                if (directionIncrease)
+                {
+                    result = report[i+1] - report[i];
+                }
+                else
+                {
+                    result = report[i] - report[i+1];
+                }
+
+                if (result < 1 || result > 3)
+                {
+                    allGood = false;
+                    break;
+                }
+            }
+
+            if (allGood)
+            {
+                safeReportsCount++;
+            }
+        }
+        
+        Console.WriteLine($"Step one result: {safeReportsCount}");
+    }
+
+    private bool reportIsValid(List<int> report, bool originalReport)
+    {
+        var directionIncrease = report[0] < report[report.Count-1];
+
+        var allGood = true;
+        var hadOneFailure = false;
+
+        for (var i = 0; i < report.Count - 1; i++)
+        {
+            var result = 0;
+            if (directionIncrease)
+            {
+                result = report[i+1] - report[i];
+            }
+            else
+            {
+                result = report[i] - report[i+1];
+            }
+
+            if (result < 1 || result > 3)
+            {
+                if (!originalReport) return false;
+                List<int> tempReport;
+                for (int j = 0; j < report.Count; j++)
+                {
+                    tempReport = report.ToList();
+                    tempReport.RemoveAt(j);
+                    if (reportIsValid(tempReport, false))
+                    {
+                        return true;
+                    }
+                }
+
+                return false;
+            }
+        }
+
+        return true;
+    }
+    
+    public void Step2()
+    {
+        var input = _inputFiles.ReadInputFileForDay(2, false);
+        var inputList = _inputFiles.SplitString(input);
+
+        var reports = new List<List<int>>();
+
+        foreach (var inputItem in inputList)
+        {
+            var inputValues = inputItem.Split(" ").Select(int.Parse).ToList();
+            reports.Add(inputValues);
+        }
+
+        var safeReportsCount = 0;
+
+        foreach (var report in reports)
+        {
+            var allGood = reportIsValid(report, true);
+
+            if (allGood)
+            {
+                safeReportsCount++;
+            }
+        }
+        
+        Console.WriteLine($"Step two result: {safeReportsCount}");
+    }
+}
